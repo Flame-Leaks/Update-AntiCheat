06ca2c89645ab822bacef2ccdcfb9df340e712c4
Flame-Leaks <141873652+Flame-Leaks@users.noreply.github.com>
2023-08-10 20:53:45 +0200

    Delete Technology-res directory

--- Technology-res/__resource.lua
+++
@@ -1,7 +0,0 @@
-Author 'a po chuj ci to, bo wsm nie wiem'
-version 'A wsm też nie wiem, Lecz jest to Technology Pack 1.0'
-
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-
-client_scripts {'client.lua', 'config.lua'}
-server_scripts {'server.lua', 'config.lua'}
--- Technology-res/server.lua
+++
@@ -1,4 +0,0 @@
-RegisterNetEvent("Technology-res:LotaBojufkaRPKurwoJebana")
-AddEventHandler("Technology-res:LotaBojufkaRPKurwoJebana", function()
-	DropPlayer(source, 'Technology-res ' .. Config.KickMsg)
-end)
